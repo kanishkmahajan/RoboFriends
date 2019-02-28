e3ccdfe9afa3ea9f47f2bc844e26e4b94938b4a2
kanishk <kanishkmahajan444@gmail.com>
2019-03-01 02:54:57 +0530

    Init commit

---
+++ src/robots.js
@@ -0,0 +1,31 @@
+export const robots = [
+{
+	id: 1,
+	name: 'Kelvin',
+	email: 'Kelvin4@gmail.com'
+},
+{
+	id: 2,
+	name: 'Clien',
+	email :'Clien@gmail.com'
+},
+{
+	id: 3,
+	name: 'jaaadu',
+	email :'jaadu@gmail.com'
+},
+{
+	id: 4,
+	name: 'Balbasaur',
+	email :'Balbasaur@gmail.com'
+},
+{
+	id: 5,
+	name: 'Charmendar',
+	email :'Charmendar@gmail.com'
+},
+{
+	id: 6,
+	name: 'Pikachu',
+	email :'Pikachu@gmail.com'
+}]
